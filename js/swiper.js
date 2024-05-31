5c13f3f3924aa557bee16b8fbbc0904ae9d720a3
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-05-31 13:42:46 +0330

    swiper config

--- js/swiper.js
+++ js/swiper.js
@@ -4,19 +4,19 @@ const swiper = new Swiper('.swiper', {
     loop: true,
   
     // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
   
     // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
   
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
   });
   
