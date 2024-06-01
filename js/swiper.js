c77bb969bf85928aca9d6958d8eeba24ff240aef
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-06-01 15:49:24 +0330

    swiper config

--- js/swiper.js
+++ js/swiper.js
@@ -2,7 +2,14 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    // loopedSlides: 1,
+    slidesPerView: 3,
+    // nextButton: '.swiper-button-next',
+    // prevButton: '.swiper-button-prev',
+    spaceBetween: 10,
+    centeredSlides: false,
+
+
     // auto play option
     autoplay: {
       delay: 3000,
